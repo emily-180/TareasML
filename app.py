6d16fa3952ec0df594fbac360169087f61c8f2e5
emily-180 <emily.moreira.8@gmail.com>
2025-09-16 11:55:01 -0500

    Anandir regresionLinear

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import Flask, render_template, redirect, url_for, request
+import regresionLinear
 app = Flask(__name__)
 
 @app.route("/")  
@@ -25,6 +25,23 @@ def caso3():
 @app.route("/caso4")
 def caso4():
     return render_template("caso4.html")
+
+@app.route("/conceptoLineal")
+def conceptoLineal():
+    return render_template("conceptoLineal.html")
+
+@app.route("/praticoLineal", methods=["GET"])
+def praticoLineal_form():
+    return render_template("praticoLineal.html", grafico=None, result=None)
+
+@app.route("/praticoLineal", methods=["POST"])
+def praticoLineal_result():
+    cantidad = float(request.form["cantidad"])
+    costo = float(request.form["costo"])
+    calculateResult = regresionLinear.calcularPrecio(cantidad, costo)
+    grafico = regresionLinear.generarGrafico(cantidad, costo, calculateResult)
+
+    return render_template("praticoLineal.html", result=calculateResult, grafico=grafico)
 
 if __name__ == "__main__":
     app.run(debug=True)
